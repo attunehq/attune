8124da3a86c7cb62b39b041334ffeec60650cc75
Claudia Pellegrino <claui@users.noreply.github.com>
2025-04-25 01:11:32 +0200

    Accept signing key ID as alternative to key file
    In automated build environments, it might be acceptable to have private
    signing key material available in file form, so it should be fine to
    hand over the key to Attune for signing.
    
    However, in some scenarios, that’s not feasible. For example, a senior
    package developer might be evaluating Attune for their team, and they
    keep their signing key on a personal YubiKey, so they can’t export their
    private key material for Attune even if they were willing to (and that’s
    a big if).
    
    Or a package maintainer might be using GPG agent forwarding, e.g.
    because they’re running the Attune CLI in a container, or for
    troubleshooting a multi-architecture build on a remote build node.
    
    To support these scenarios, add a `--signing-key-id` switch to the CLI
    as an alternative to `--signing-key-file`.

--- cli/cmd/attune/repository.go
+++ cli/cmd/attune/repository.go
@@ -6,14 +6,10 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"strings"
-	"syscall"
 	"text/tabwriter"
 	"time"
 
-	"github.com/ProtonMail/gopenpgp/v3/crypto"
 	"github.com/spf13/cobra"
-	"golang.org/x/term"
 )
 
 func repoCmd() *cobra.Command {
@@ -39,12 +35,7 @@ func repoCmd() *cobra.Command {
 	statusRepositoryCmd.Flags().IntP("repo-id", "r", 0, "ID of the repository")
 	statusRepositoryCmd.MarkFlagRequired("repo-id")
 
-	syncRepositoryCmd.Flags().IntP("repo-id", "r", 0, "ID of the repository")
-	syncRepositoryCmd.MarkFlagRequired("repo-id")
-	syncRepositoryCmd.Flags().StringP("signing-key-file", "k", "", "File containing armored GPG private key for signing")
-	syncRepositoryCmd.MarkFlagRequired("signing-key-file")
-
-	cmd.AddCommand(createRepositoryCmd, listRepositoriesCmd, statusRepositoryCmd, syncRepositoryCmd, repoPkgCmd())
+	cmd.AddCommand(createRepositoryCmd, listRepositoriesCmd, statusRepositoryCmd, repoSyncCmd(), repoPkgCmd())
 	return cmd
 }
 
@@ -257,145 +248,3 @@ var statusRepositoryCmd = &cobra.Command{
 	},
 }
 
-type RepositoryIndexes struct {
-	Release string
-}
-
-type SyncRepositoryRequest struct {
-	Clearsigned string `json:"clearsigned"`
-	Detached    string `json:"detached"`
-}
-
-var syncRepositoryCmd = &cobra.Command{
-	// Other potential names: "commit", "deploy", "update", "push"?
-	Use:   "sync",
-	Short: "Synchronize unsaved changes to repository",
-	Run: func(cmd *cobra.Command, args []string) {
-		repoID, err := cmd.Flags().GetInt("repo-id")
-		if err != nil {
-			fmt.Printf("could not read --repo-id: %s\n", err)
-			os.Exit(1)
-		}
-		signingKeyFile, err := cmd.Flags().GetString("signing-key-file")
-		if err != nil {
-			fmt.Printf("could not read --signing-key-file: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Load release index for signing.
-		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/api/v0/repositories/%d/indexes", repoID), nil)
-		if err != nil {
-			fmt.Printf("could not create request to get repository indexes: %s\n", err)
-			os.Exit(1)
-		}
-		res, err := API(req)
-		if err != nil {
-			fmt.Printf("could not get repository indexes: %s\n", err)
-			os.Exit(1)
-		}
-		defer res.Body.Close()
-
-		if res.StatusCode != http.StatusOK {
-			fmt.Printf("could not get repository indexes: %s\n", res.Status)
-			os.Exit(1)
-		}
-
-		var indexes RepositoryIndexes
-		if err := json.NewDecoder(res.Body).Decode(&indexes); err != nil {
-			fmt.Printf("could not decode repository indexes: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Sign release index.
-		keyFd, err := os.Open(signingKeyFile)
-		if err != nil {
-			fmt.Printf("could not open key file: %s\n", err)
-			os.Exit(1)
-		}
-		defer keyFd.Close()
-
-		key, err := crypto.NewKeyFromReader(keyFd)
-		if err != nil {
-			fmt.Printf("could not parse key file: %s\n", err)
-			os.Exit(1)
-		}
-		locked, err := key.IsLocked()
-		if err != nil {
-			fmt.Printf("could not determine whether key is locked: %s\n", err)
-			os.Exit(1)
-		}
-		if locked {
-			fmt.Printf("Key is locked. Please enter passphrase: ")
-			var passphrase []byte
-			passphrase, err = term.ReadPassword(int(syscall.Stdin))
-			if err != nil {
-				fmt.Printf("could not read passphrase: %s\n", err)
-				os.Exit(1)
-			}
-			key, err = key.Unlock(passphrase)
-			if err != nil {
-				fmt.Printf("could not unlock key: %s\n", err)
-				os.Exit(1)
-			}
-			fmt.Println()
-		}
-
-		pgp := crypto.PGP()
-		signer, err := pgp.Sign().SigningKey(key).New()
-		if err != nil {
-			fmt.Printf("could not create signer: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Notice the trimmed newline. This is apparently a long-standing
-		// compatibility bug in GPG cleartext signing. See:
-		// - https://lists.gnupg.org/pipermail/gnupg-devel/1999-September/016016.html
-		// - https://dev.gnupg.org/T7106
-		clearsigned, err := signer.SignCleartext([]byte(strings.TrimSuffix(indexes.Release, "\n")))
-		if err != nil {
-			fmt.Printf("could not clearsign release index: %s\n", err)
-			os.Exit(1)
-		}
-		detached, err := signer.Sign([]byte(indexes.Release), crypto.Armor)
-		if err != nil {
-			fmt.Printf("could not detached sign release index: %s\n", err)
-			os.Exit(1)
-		}
-
-		// Start synchronization.
-		reqBody := SyncRepositoryRequest{
-			Clearsigned: string(clearsigned),
-			Detached:    string(detached),
-		}
-
-		jsonBody, err := json.Marshal(reqBody)
-		if err != nil {
-			fmt.Printf("could not marshal SyncRepositoryRequest: %s\n", err)
-			os.Exit(1)
-		}
-
-		req, err = http.NewRequest(
-			http.MethodPost,
-			fmt.Sprintf("/api/v0/repositories/%d/sync", repoID),
-			bytes.NewReader(jsonBody),
-		)
-		if err != nil {
-			fmt.Printf("could not create request for starting synchronization: %s\n", err)
-			os.Exit(1)
-		}
-		req.Header.Set("Content-Type", "application/json")
-		res, err = API(req)
-		if err != nil {
-			fmt.Printf("could not start synchronization: %s\n", err)
-			os.Exit(1)
-		}
-		defer res.Body.Close()
-
-		if res.StatusCode != http.StatusOK {
-			fmt.Printf("could not start synchronization: %s\n", res.Status)
-			os.Exit(1)
-		}
-
-		fmt.Println("Synchronization completed!")
-	},
-}
