7f626f1c9583772868339f6466b8823f8bd3acda
Eliza Zhang <eliza@elizazhang.com>
2025-04-24 11:51:08 -0700

    nit: 4-digit RFDs

--- docker/minio/entrypoint.sh
+++ docker/minio/entrypoint.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# This creates a default bucket for the Minio instance, which is required by
+# Attune.
+#
 # See: https://github.com/minio/minio/issues/4769
 mkdir /data/attune-dev-0
 exec minio server /data --console-address ":9001"
