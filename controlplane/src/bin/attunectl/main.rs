7504bcef9091dd348c47b06442f6fd37c9059273
Eliza Zhang <eliza@elizazhang.com>
2025-07-02 01:31:50 -0700

    feat: Simplify attunectl

--- controlplane/src/bin/attunectl/main.rs
+++ controlplane/src/bin/attunectl/main.rs
@@ -14,57 +14,14 @@ struct CLI {
 
 #[derive(Subcommand)]
 enum Command {
-    #[command(name = "tenant", about = "Manage tenants")]
-    Tenant(TenantCommand),
+    #[command(name = "init", about = "Initialize Attune")]
+    Init,
     #[command(name = "token", about = "Manage API tokens")]
     Token(TokenCommand),
 }
 
 #[derive(Args)]
-struct TenantCommand {
-    #[command(subcommand)]
-    subcommand: TenantSubcommand,
-}
-
-#[derive(Subcommand)]
-enum TenantSubcommand {
-    #[command(name = "add", about = "Add a new tenant")]
-    Add {
-        #[arg(short = 'd', long, help = "Display name for the tenant")]
-        display_name: String,
-        #[arg(short = 'u', long, help = "Subdomain for the tenant")]
-        subdomain: String,
-        #[arg(
-            short = 's',
-            help = "S3 prefix for the tenant (default: SHA-256 hash of subdomain)"
-        )]
-        s3_prefix: Option<String>,
-    },
-    #[command(name = "list", alias = "ls", about = "List all tenants")]
-    List,
-    #[command(name = "edit", about = "Edit a tenant")]
-    Edit {
-        #[arg(short = 't', long, help = "ID of tenant to edit")]
-        tenant_id: i64,
-        #[arg(short = 'd', long, help = "New display name for the tenant")]
-        display_name: Option<String>,
-        #[arg(short = 'u', long, help = "New subdomain for the tenant")]
-        subdomain: Option<String>,
-        #[arg(short = 's', long, help = "New S3 prefix for the tenant")]
-        s3_prefix: Option<String>,
-    },
-    #[command(name = "remove", alias = "rm", about = "Remove a tenant")]
-    Remove {
-        #[arg(short = 't', long, help = "ID of tenant to remove")]
-        tenant_id: i64,
-    },
-}
-
-#[derive(Args)]
 struct TokenCommand {
-    #[arg(short = 't', long, help = "ID of tenant to manage")]
-    tenant_id: i64,
-
     #[command(subcommand)]
     subcommand: TokenSubcommand,
 }
@@ -118,102 +75,42 @@ async fn main() {
 
     // Execute command.
     match cli.command {
-        Command::Tenant(command) => handle_tenant(command, db).await,
+        Command::Init => handle_init(db).await,
         Command::Token(command) => handle_token(command, db).await,
     }
 }
 
-async fn handle_tenant(command: TenantCommand, db: PgPool) {
-    match command.subcommand {
-        TenantSubcommand::Add {
-            display_name,
-            s3_prefix,
-            subdomain,
-        } => {
-            let tenant = sqlx::query!(
+async fn handle_init(db: PgPool) {
+    println!("Initializing...");
+    match sqlx::query!("SELECT id FROM attune_tenant WHERE id = 1")
+        .fetch_optional(&db)
+        .await
+        .expect("could not find local tenant")
+    {
+        Some(_) => {
+            println!("Attune is already initialized");
+        }
+        None => {
+            sqlx::query!(
                 "INSERT INTO attune_tenant (display_name, subdomain, s3_prefix) VALUES ($1, $2, $3) RETURNING id",
-                display_name,
-                subdomain,
-                s3_prefix.unwrap_or(hex::encode(Sha256::digest(&subdomain))),
+                "local",
+                "",
+                "attune",
             )
             .fetch_one(&db)
             .await
             .expect("could not add tenant");
-            println!("Added tenant with ID {}", tenant.id);
-        }
-        TenantSubcommand::List => {
-            let tenants =
-                sqlx::query!("SELECT id, display_name, subdomain, s3_prefix FROM attune_tenant")
-                    .fetch_all(&db)
-                    .await
-                    .expect("could not list tenants");
-            let mut builder = tabled::builder::Builder::new();
-            builder.push_record([
-                "ID".to_string(),
-                "Display Name".to_string(),
-                "Subdomain".to_string(),
-                "S3 Prefix".to_string(),
-            ]);
-            for tenant in tenants {
-                builder.push_record([
-                    tenant.id.to_string(),
-                    tenant.display_name,
-                    tenant.subdomain,
-                    tenant.s3_prefix,
-                ]);
-            }
-            let table = builder.build();
-            println!("{}", table.to_string());
-        }
-        TenantSubcommand::Edit {
-            tenant_id,
-            display_name,
-            subdomain,
-            s3_prefix,
-        } => {
-            let updated = sqlx::query!(
-                r#"
-                UPDATE attune_tenant
-                SET display_name = COALESCE($2, old_tenant.display_name),
-                    subdomain = COALESCE($3, old_tenant.subdomain),
-                    s3_prefix = COALESCE($4, old_tenant.s3_prefix)
-                FROM (
-                    SELECT display_name,
-                           subdomain,
-                           s3_prefix
-                    FROM attune_tenant
-                    WHERE id = $1
-                ) AS old_tenant
-                WHERE id = $1
-                RETURNING id, attune_tenant.display_name, attune_tenant.subdomain, attune_tenant.s3_prefix
-                "#,
-                tenant_id,
-                display_name,
-                subdomain,
-                s3_prefix,
-            )
-            .fetch_one(&db)
-            .await
-            .expect("could not edit tenant");
-            println!("Edited tenant with ID {} to {:?}", tenant_id, updated);
-        }
-        TenantSubcommand::Remove { tenant_id } => {
-            sqlx::query!("DELETE FROM attune_tenant WHERE id = $1", tenant_id)
-                .execute(&db)
-                .await
-                .expect("could not remove tenant");
-            println!("Removed tenant with ID {}", tenant_id);
+            println!("Done!");
         }
     }
 }
 
 async fn handle_token(command: TokenCommand, db: PgPool) {
-    let tenant_id = command.tenant_id;
     match command.subcommand {
         TokenSubcommand::Add { name, value } => {
             let token = sqlx::query!(
                 "INSERT INTO attune_tenant_api_token (tenant_id, name, token) VALUES ($1, $2, $3) RETURNING id",
-                tenant_id,
+                1,
                 name,
                 Sha256::digest(value).as_slice().to_vec(),
             )
@@ -225,7 +122,7 @@ async fn handle_token(command: TokenCommand, db: PgPool) {
         TokenSubcommand::List => {
             let tokens = sqlx::query!(
                 "SELECT id, name, token FROM attune_tenant_api_token WHERE tenant_id = $1",
-                tenant_id
+                1
             )
             .fetch_all(&db)
             .await
