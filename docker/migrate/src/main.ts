9beb90e395da5026e8da8c29f9efe9075bd8d330
Fred Heinecke <fred.heinecke@goteleport.com>
2025-07-28 16:53:38 -0500

    Switch conn string parsing to `URL`

--- docker/migrate/src/main.ts
+++ docker/migrate/src/main.ts
@@ -17,14 +17,17 @@ import { buildIAMAuthenticationConnectionString } from "./rds.ts"
  * * Any other value uses authentication provided via the connection string (e.g. password, password file, mTLS, etc.)
  */
 async function main() {
-    const authMethod = process.env["ATTUNE_MIGRATION_AUTHENTICATION_METHOD"];
-
     const providedConnectionString = process.env["ATTUNE_DATABASE_URL"];
     if (!providedConnectionString) throw new Error("ATTUNE_DATABASE_URL was not provided");
 
+    const authMethod = process.env["ATTUNE_MIGRATION_AUTHENTICATION_METHOD"];
     switch (authMethod) {
         case "rds-iam":
+            console.log("Using AWS RDS IAM authentication");
             process.env["ATTUNE_DATABASE_URL"] = await buildIAMAuthenticationConnectionString(providedConnectionString);
+            break
+        default:
+            console.log("Using connection string authentication");
     }
 
     const logConnectionString = process.env["ATTUNE_MIGRATION_LOG_CONNECTION_STRING_INSECURE_DO_NOT_SET_THIS"];
@@ -32,7 +35,7 @@ async function main() {
         console.log(process.env["ATTUNE_DATABASE_URL"]);
 
     // `execve` requires a filepath to execute. Use `env`, masquerading as `npm`
-    // to find and execute `npm` with the providedprocess.environment variables.
+    // to find and execute `npm` with the provided `process.env` variables.
     // This will not return unless there is an error loading/jumping to the
     // `env` code. The current process is replaced with `env`, which in turn
     // replaces itself with `npm`.
--- docker/migrate/src/rds.ts
+++ docker/migrate/src/rds.ts
@@ -1,6 +1,5 @@
 import "process";
 import { Signer } from "@aws-sdk/rds-signer";
-import { parse as parseConnectionString } from 'pg-connection-string';
 
 /**
  * Given the provided connection string, generate a password using AWS IAM Authentication add it to the connection string.
@@ -11,16 +10,18 @@ import { parse as parseConnectionString } from 'pg-connection-string';
  * @returns An identical connection string, but with the password included
  */
 export async function buildIAMAuthenticationConnectionString(connectionString: string): Promise<string> {
-    const connectionInfo = parseConnectionString(connectionString);
+    const connectionURL = new URL(connectionString);
+    if (connectionURL.hostname === "") connectionURL.hostname = process.env["PGHOST"] ?? "localhost"
+    if (connectionURL.port === "") connectionURL.port = process.env["PGPORT"] ?? "5432"
+    if (connectionURL.username === "") connectionURL.username = process.env["PGUSER"] ?? "postgres"
 
     const signer = new Signer({
-        hostname: connectionInfo.host ?? process.env["PGHOST"] ?? "localhost",
-        port: connectionInfo.port ? +connectionInfo.port : process.env["PGPORT"] ? + process.env["PGPORT"] : 5432,
-        username: connectionInfo.user ?? process.env["PGUSER"] ?? "postgres",
+        hostname: connectionURL.hostname,
+        port: +connectionURL.port,
+        username: connectionURL.username,
         profile: process.env["ATTUNE_RDS_IAM_PROFILE"] ?? null
     })
 
-    // Append to the connection string to avoid needing to rebuild it
-    const token = await signer.getAuthToken();
-    return `${connectionString}${connectionInfo.options ? "?" : "&"}password=${encodeURIComponent(token)}`;
+    connectionURL.password = await signer.getAuthToken();
+    return connectionURL.toString();
 }
