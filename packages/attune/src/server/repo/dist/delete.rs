719f2dbbf0fe415ed6918584e3113ac5b207dabc
Jessica Black <me@jessica.black>
2025-07-31 11:57:45 -0700

    WIP: known key based deletion

--- packages/attune/src/server/repo/dist/delete.rs
+++ packages/attune/src/server/repo/dist/delete.rs
@@ -54,7 +54,26 @@ pub async fn handler(
             .build()
     })?;
 
-    // Delete the distribution (cascade will handle related records)
+    // First, query all components and their architectures for this distribution.
+    // We use this to delete the actual files in S3 after we delete the distribution in the database.
+    let components = sqlx::query!(
+        r#"
+        SELECT
+            c.name as component_name,
+            pi.architecture::text as architecture
+        FROM debian_repository_release r
+        JOIN debian_repository_component c ON c.release_id = r.id
+        JOIN debian_repository_index_packages pi ON pi.component_id = c.id
+        WHERE r.repository_id = $1 AND r.distribution = $2
+        "#,
+        repo.id,
+        distribution_name,
+    )
+    .fetch_all(&mut *tx)
+    .await
+    .unwrap();
+
+    // Cascade will handle related records when deleting the distribution.
     let result = sqlx::query!(
         r#"
         DELETE FROM debian_repository_release
@@ -67,70 +86,70 @@ pub async fn handler(
     .await
     .unwrap();
 
+    // If no rows were affected, the distribution was already deleted.
+    // We return an OK response here because the user's goal was accomplished regardless.
     if result.rows_affected() == 0 {
-        return Err(ErrorResponse::builder()
-            .status(axum::http::StatusCode::NOT_FOUND)
-            .error("DIST_NOT_FOUND")
-            .message("distribution not found")
-            .build());
+        return Ok(Json(DeleteDistributionResponse::default()));
     }
 
     tx.commit().await.unwrap();
 
-    // Clean up S3 objects for this distribution using prefix-based deletion.
+    // Clean up S3 objects for this distribution based on known paths.
     //
     // Note: We don't delete the actual package files (under `/packages/`) because
     // they might be referenced by other distributions in the same or different repositories.
     //
     // As a future improvement we could do garbage collection to delete the actual package files.
-    let s3_prefix = format!("{}/dists/{}/", repo.s3_prefix, distribution_name);
 
-    // The continuation token is used to paginate through all the objects.
-    let mut continuation_token = None;
-    loop {
-        let res = state
-            .s3
-            .list_objects_v2()
-            .bucket(&repo.s3_bucket)
-            .prefix(&s3_prefix)
-            .set_continuation_token(continuation_token)
-            .send()
-            .await
-            .unwrap();
+    // Build the list of S3 keys to delete based on the known repository structure
+    let prefix = format!("{}/dists/{}", repo.s3_prefix, distribution_name);
+    let mut keys = vec![
+        format!("{prefix}/Release"),
+        format!("{prefix}/Release.gpg"),
+        format!("{prefix}/InRelease"),
+    ];
 
-        let targets = res
-            .contents()
-            .into_iter()
-            .filter_map(|obj| obj.key())
+    for record in components {
+        let component_prefix = format!(
+            "{}/{}/binary-{}",
+            prefix,
+            record.component_name,
+            record.architecture.as_ref().unwrap().replace('_', "-")
+        );
+
+        // Add the uncompressed Packages file (currently we only create uncompressed indexes)
+        // TODO: When compressed indexes are implemented, add their deletion here
+        keys.push(format!("{component_prefix}/Packages"));
+        keys.push(format!("{component_prefix}/Release"));
+    }
+
+    // Delete the objects in batches (S3 delete_objects supports up to 1000 objects per request)
+    for chunk in keys.chunks(1000) {
+        let objects: Vec<_> = chunk
+            .iter()
             .map(|key| {
                 aws_sdk_s3::types::ObjectIdentifier::builder()
                     .key(key)
                     .build()
                     .unwrap()
             })
-            .collect::<Vec<_>>();
-        if !targets.is_empty() {
-            let delete = aws_sdk_s3::types::Delete::builder()
-                .set_objects(Some(targets))
-                .build()
-                .unwrap();
+            .collect();
 
-            let deleted = state
-                .s3
-                .delete_objects()
-                .bucket(&repo.s3_bucket)
-                .delete(delete)
-                .send()
-                .await;
-            if let Err(err) = deleted {
-                tracing::error!("Failed to delete objects: {err:?}");
-            }
-        }
+        let delete = aws_sdk_s3::types::Delete::builder()
+            .set_objects(Some(objects))
+            .build()
+            .unwrap();
 
-        if res.is_truncated() == Some(true) {
-            continuation_token = res.next_continuation_token().map(|s| s.to_string());
-        } else {
-            break;
+        let result = state
+            .s3
+            .delete_objects()
+            .bucket(&repo.s3_bucket)
+            .delete(delete)
+            .send()
+            .await;
+
+        if let Err(err) = result {
+            tracing::error!("Failed to delete objects: {err:?}");
         }
     }
 
