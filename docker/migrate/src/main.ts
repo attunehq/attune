cdde95992b6cad5c342b23dc0abe59c6011d43a3
Jessica Black <me@jessica.black>
2025-08-01 17:25:37 -0700

    Clean up with minimist and constants

--- docker/migrate/src/main.ts
+++ docker/migrate/src/main.ts
@@ -1,40 +1,56 @@
 import "process";
+import minimist from "minimist";
 import { buildIAMAuthenticationConnectionString } from "./rds.js"
 
+const ENV_DANGEROUSLY_LOG_CONNECTION_STRING = "ATTUNE_MIGRATION_LOG_CONNECTION_STRING_INSECURE_DO_NOT_SET_THIS";
+const ENV_ATTUNE_DATABASE_URL = "ATTUNE_DATABASE_URL";
+
+const ARG_AUTH_METHOD = "auth-method";
+const ARG_AUTH_METHOD_RDSIAM = "rds-iam";
+
+const LOG_CONNECTION_STRING = process.env[ENV_DANGEROUSLY_LOG_CONNECTION_STRING] === "true";
+
 /**
- * Runs Attune database migrations. The environment variable `ATTUNE_MIGRATION_AUTHENTICATION_METHOD`
- * can be specified to control how authentication is handled. The `ATTUNE_DATABASE_URL` variable
- * provides the connection string.
+ * Runs Attune database migrations. The `ATTUNE_DATABASE_URL` variable provides the connection string.
  *
- * This should generally be executed via `npm run migrate:autoauth`.
+ * The `auth-method` argument can be used to specify the authentication method. The default if not set
+ * is to use authentication provided via the connection string (e.g. password, password file, mTLS, etc.).
  *
- * The `ATTUNE_MIGRATION_LOG_CONNECTION_STRING_INSECURE_DO_NOT_SET_THIS` variable can be set to `true`
+ * `auth-method` values:
+ * - `rds-iam` Use [AWS RDS IAM authentication](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.IAMDBAuth.html)
+ *
+ * ## Debugging
+ *
+ * Set the `ATTUNE_MIGRATION_LOG_CONNECTION_STRING_INSECURE_DO_NOT_SET_THIS` variable to `true`
  * to log the connection string. This may include the plaintext password, and should only be used with
  * temporary credentials for debugging purposes.
- *
- * `ATTUNE_MIGRATION_AUTHENTICATION_METHOD` values:
- * * `rds-iam` Use [AWS RDS IAM authentication](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/UsingWithRDS.IAMDBAuth.html)
- * * Any other value uses authentication provided via the connection string (e.g. password, password file, mTLS, etc.)
  */
 async function main() {
-    const providedConnectionString = process.env["ATTUNE_DATABASE_URL"];
+    const providedConnectionString = process.env[ENV_ATTUNE_DATABASE_URL];
     if (!providedConnectionString) {
         throw new Error("ATTUNE_DATABASE_URL was not provided");
     }
 
-    const authMethod = process.env["ATTUNE_MIGRATION_AUTHENTICATION_METHOD"];
-    switch (authMethod) {
-        case "rds-iam":
-            console.log("Using AWS RDS IAM authentication");
-            process.env["ATTUNE_DATABASE_URL"] = await buildIAMAuthenticationConnectionString(providedConnectionString);
-            break
-        default:
-            console.log("Using connection string authentication");
+    const argv = minimist(process.argv.slice(2));
+    const authMethod = argv[ARG_AUTH_METHOD];
+
+    if (LOG_CONNECTION_STRING) {
+        console.log("Using auth-method: ", authMethod);
+        console.log("Provided ATTUNE_DATABASE_URL: ", providedConnectionString);
     }
 
-    const logConnectionString = process.env["ATTUNE_MIGRATION_LOG_CONNECTION_STRING_INSECURE_DO_NOT_SET_THIS"];
-    if (logConnectionString === "true") {
-        console.log(process.env["ATTUNE_DATABASE_URL"]);
+    // We set the `ATTUNE_DATABASE_URL` environment variable to the resolved connection string
+    // so that when we run `npm run migrate` below it will use the resolved connection string.
+    switch (authMethod) {
+        case ARG_AUTH_METHOD_RDSIAM:
+            console.log("Using AWS RDS IAM authentication");
+            process.env[ENV_ATTUNE_DATABASE_URL] = await buildIAMAuthenticationConnectionString(providedConnectionString);
+            if (LOG_CONNECTION_STRING) {
+                console.log("Resolved ATTUNE_DATABASE_URL: ", process.env[ENV_ATTUNE_DATABASE_URL]);
+            }
+            break;
+        default:
+            console.log("Using connection string authentication");
     }
 
     // `execve` requires a filepath to execute. Use `env`, masquerading as `npm`
