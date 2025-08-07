2ff01c6b35e4b8fc02dd813ca82c1f533a9ef2ef
Eliza Zhang <eliza@elizazhang.com>
2025-08-07 15:10:59 -0700

    cr: Run formatter

--- packages/attune/src/apt/mod.rs
+++ packages/attune/src/apt/mod.rs
@@ -2,6 +2,6 @@ mod package;
 mod packages_index;
 mod release;
 
-pub use package::{Package, PublishedPackage, PackageByMeta, PublishedPackageByMeta};
+pub use package::{Package, PackageByMeta, PublishedPackage, PublishedPackageByMeta};
 pub use packages_index::{PackagesIndex, PackagesIndexMeta};
 pub use release::{ReleaseFile, ReleaseMeta};
