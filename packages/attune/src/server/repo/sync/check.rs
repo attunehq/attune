4fd7f607d258ff11f182030f04d2815b5923af22
Eliza Zhang <eliza@attunehq.com>
2025-08-07 21:15:24 -0700

    feat: Implement resynchronization (#114)
    * refactor: Factor out effects from resync
    
    * feat: Resync endpoint
    
    * feat: apt dist resync subcommand
    
    * ci: Clippy and formatting
    
    * ci: Update sqlx

--- packages/attune/src/server/repo/sync/check.rs
+++ packages/attune/src/server/repo/sync/check.rs
@@ -2,7 +2,8 @@ use axum::{
     Json,
     extract::{Path, State},
 };
-use tracing::instrument;
+use serde::{Deserialize, Serialize};
+use tracing::{debug, instrument};
 
 use crate::{
     api::{ErrorResponse, TenantID},
@@ -10,10 +11,16 @@ use crate::{
         ServerState,
         repo::{
             decode_repo_name,
-            sync::{InconsistentObjects, check_consistency},
+            sync::{InconsistentSummary, check_s3_consistency, query_repository_state},
         },
     },
 };
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct CheckConsistencyResponse {
+    #[serde(flatten)]
+    pub status: InconsistentSummary,
+}
 
 #[axum::debug_handler]
 #[instrument(skip(state))]
@@ -21,21 +28,26 @@ pub async fn handler(
     State(state): State<ServerState>,
     tenant_id: TenantID,
     Path((repo_name, release_name)): Path<(String, String)>,
-) -> Result<Json<InconsistentObjects>, ErrorResponse> {
+) -> Result<Json<CheckConsistencyResponse>, ErrorResponse> {
     // The repository name in the path is percent-encoded.
     let repo_name = decode_repo_name(&repo_name)?;
     let release_name = decode_repo_name(&release_name)?;
 
+    // Get current repository state.
     let mut tx = state.db.begin().await.unwrap();
     sqlx::query!("SET TRANSACTION ISOLATION LEVEL SERIALIZABLE")
         .execute(&mut *tx)
         .await
         .unwrap();
-
-    let inconsistent_objects =
-        check_consistency(&mut tx, state.s3, &tenant_id, repo_name, release_name).await?;
-
+    let repo = query_repository_state(&mut tx, &tenant_id, repo_name, release_name).await?;
     tx.commit().await.unwrap();
+    debug!(?repo, "loaded repository state");
 
-    Ok(Json(inconsistent_objects))
+    // Check which S3 objects are inconsistent.
+    let inconsistent_objects = check_s3_consistency(&state.s3, repo).await?;
+    debug!(?inconsistent_objects, "checked S3");
+
+    Ok(Json(CheckConsistencyResponse {
+        status: InconsistentSummary::from(&inconsistent_objects),
+    }))
 }
