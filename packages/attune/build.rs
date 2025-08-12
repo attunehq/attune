444044f9562d955ce3e129685ecfbdb275d88cc6
Eliza Zhang <eliza@elizazhang.com>
2025-08-11 21:33:48 -0700

    ci: Update env for integration tests

--- packages/attune/build.rs
+++ packages/attune/build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    if cfg!(debug_assertions) {
+    if cfg!(test) {
         println!("cargo::rerun-if-env-changed=ATTUNE_DATABASE_URL");
         if let Ok(attune_database_url) = std::env::var("ATTUNE_DATABASE_URL") {
             println!("cargo::rustc-env=DATABASE_URL={attune_database_url}");
