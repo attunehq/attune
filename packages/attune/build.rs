416510af7199569b69886503568e110c4109d739
Eliza Zhang <eliza@elizazhang.com>
2025-08-11 22:24:44 -0700

    Apparently, cfg!(test) is false when --release

--- packages/attune/build.rs
+++ packages/attune/build.rs
@@ -1,8 +1,7 @@
 fn main() {
-    if cfg!(test) {
-        println!("cargo::rerun-if-env-changed=ATTUNE_DATABASE_URL");
-        if let Ok(attune_database_url) = std::env::var("ATTUNE_DATABASE_URL") {
-            println!("cargo::rustc-env=DATABASE_URL={attune_database_url}");
-        }
+    println!("cargo::rerun-if-env-changed=ATTUNE_DATABASE_URL");
+    if let Ok(attune_database_url) = std::env::var("ATTUNE_DATABASE_URL") {
+        // This sets the environment variable for sqlx's test macros.
+        println!("cargo::rustc-env=DATABASE_URL={attune_database_url}");
     }
 }
