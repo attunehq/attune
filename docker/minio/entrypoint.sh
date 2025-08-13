5424378c3ab5fcff9ab3736b8ce8d92f6866fb37
Eliza Zhang <eliza@elizazhang.com>
2025-08-13 14:48:03 -0700

    refactor: Correctly handle signals in Minio container

--- docker/minio/entrypoint.sh
+++ docker/minio/entrypoint.sh
@@ -11,4 +11,8 @@ sleep 1
 mc alias set attune http://127.0.0.1:9000 attuneminio attuneminio
 mc mb --ignore-existing attune/attune-dev-0
 mc anonymous set download attune/attune-dev-0
-wait $MINIO_SERVER_PID
+
+# We `exec` instead of just `wait`ing on the PID so that signals propagate
+# correctly.
+kill $MINIO_SERVER_PID
+exec minio server /data --console-address ":9001"
