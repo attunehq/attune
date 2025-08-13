92a66e167354820c566d7b4683a66f5b3263d1a4
Eliza Zhang <eliza@elizazhang.com>
2025-08-13 16:44:17 -0700

    ci: Fix test flags and permissions

--- docker/minio/entrypoint.sh
+++ docker/minio/entrypoint.sh
@@ -10,7 +10,9 @@ MINIO_SERVER_PID=$!
 sleep 1
 mc alias set attune http://127.0.0.1:9000 attuneminio attuneminio
 mc mb --ignore-existing attune/attune-dev-0
-mc anonymous set download attune/attune-dev-0
+# This needs to be readable for the E2E test to install packages, and writeable
+# for integration tests to upload objects.
+mc anonymous set public attune/attune-dev-0
 
 # We `exec` instead of just `wait`ing on the PID so that signals propagate
 # correctly.
