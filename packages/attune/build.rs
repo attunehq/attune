2e17a2efa46da0eb827d45a260cd78508e9d8dda
Eliza Zhang <eliza@elizazhang.com>
2025-08-18 22:51:34 -0700

    test: E2E tests can now be run on Docker or host

--- packages/attune/build.rs
+++ packages/attune/build.rs
@@ -1,5 +1,7 @@
 fn main() {
     println!("cargo::rerun-if-env-changed=ATTUNE_DATABASE_URL");
+    println!("cargo::rerun-if-env-changed=E2E_DOCKER");
+    println!("cargo::rerun-if-env-changed=E2E_HOST");
     if let Ok(attune_database_url) = std::env::var("ATTUNE_DATABASE_URL") {
         // This sets the environment variable for sqlx's test macros.
         println!("cargo::rustc-env=DATABASE_URL={attune_database_url}");
